e8094aa909bcc6c8687bd17ee3820fe8ec0a0fed
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-22 21:43:04 +0600

    post preview added on homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,7 +1,9 @@
 import FeaturedProject from "~/components/FeaturedProject";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
+import type { PostMeta } from "~/types";
 import AboutPreview from "~/components/AboutPreview";
+import LatestPost from "~/components/LatestPost";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,21 +14,34 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({
   request,
-}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+  const url = new URL(request.url);
+  const [projectRes, postRes] = await Promise.all([
+    fetch(`${import.meta.env.VITE_API_URL}/projects`),
+    fetch(new URL("/posts-meta.json", url)),
+  ]);
 
-  return { projects: data };
+  if (!projectRes.ok || !postRes.ok) {
+    throw new Error("Failed to fetch projects of posts");
+  }
+
+  const [projects, posts] = await Promise.all([
+    projectRes.json(),
+    postRes.json(),
+  ]);
+
+  return { projects, posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
+  const { projects, posts } = loaderData;
   console.log(projects);
   return (
     <>
       <section>
         <FeaturedProject projects={projects} count={2} />
         <AboutPreview />
+        <LatestPost posts={posts} />
       </section>
     </>
   );
